216b6abc6fc620bb461ef09ccbf0c7bab52e5945
Fagner Lisboa <fnlisboa2016@gmail.com>
2024-07-14 18:22:46 -0300

    dados do formulário / print na tela

--- src/app/register/form-pessoa-juridica/form-pessoa-juridica.component.ts
+++ src/app/register/form-pessoa-juridica/form-pessoa-juridica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-pessoa-juridica',
@@ -6,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-pessoa-juridica.component.scss']
 })
 export class FormPessoaJuridicaComponent implements OnInit {
+    pessoaJuridica = {
+      nome: '',
+      email: '',
+      telefone: '',
+      cnpj: '',
+      razaoSocial: ''
+    };
 
-  constructor() { }
+    submitted = false;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(): void {}
+  onSubmit(): void {
+    this.submitted = true;
+    console.log(this.pessoaJuridica)
+  }
 
+  goBack(): void {
+    this.router.navigate(['/register']);
+  }
 }
